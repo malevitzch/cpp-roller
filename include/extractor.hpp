583a9e6b9afa05fa536f86247c65d24405474b05
malevitzch <malevitzch@gmail.com>
2026-02-05 15:02:27 +0100

    Added removing quote includes from a file

--- include/extractor.hpp
+++ include/extractor.hpp
@@ -1,3 +1,4 @@
 #include <string>
 #include <vector>
+std::string remove_quote_includes(const std::string& buffer);
 std::vector<std::string> extract_includes(std::string filename);
--- src/extractor.cpp
+++ src/extractor.cpp
@@ -3,14 +3,16 @@
 #include <sstream>
 #include <regex>
 
+
+static const std::regex include_re(R"==(#include[ ]*"([-A-Za-z0-9_.]*)")==", std::regex::extended);
+
 std::vector<std::string> get_quote_includes(const std::string& buffer) {
   // This uses std::regex which is notoriously slow but the regex is linear
   // and simple enough that matching should be fast even on a bad backtracking
   // engine implementation (it has no "|" or suspicious "+" usage)
-  static const std::regex inc_re(R"==(#include[ ]*"([-A-Za-z0-9_.]*)")==", std::regex::extended);
 
   std::vector<std::string> results;
-  auto begin = std::sregex_iterator(buffer.begin(), buffer.end(), inc_re);
+  auto begin = std::sregex_iterator(buffer.begin(), buffer.end(), include_re);
   auto end = std::sregex_iterator();
 
   for(std::sregex_iterator it = begin; it != end; it++) {
@@ -18,6 +20,9 @@ std::vector<std::string> get_quote_includes(const std::string& buffer) {
     results.push_back(match[1].str());
   }
   return results;
+}
+std::string remove_quote_includes(const std::string& buffer) {
+  return std::regex_replace(buffer, include_re, "");
 }
 
 std::vector<std::string> extract_includes(std::string filename) {
@@ -28,5 +33,6 @@ std::vector<std::string> extract_includes(std::string filename) {
   std::stringstream buffer;
   buffer << input.rdbuf();
   std::string file = buffer.str();
+
   return get_quote_includes(file);
 }
