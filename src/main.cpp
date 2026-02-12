d08e918c0d5978307b7237507a74a1aff49efe65
malevitzch <malevitzch@gmail.com>
2026-02-12 15:03:05 +0100

    Implemented basic command line argument parsing, output can now be named by the user and multiple source files can be given

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
 #include <vector>
 #include <filesystem>
-#include "extractor.hpp"
 #include "roller.hpp"
 
 int main(int argc, char** argv) {
   if(argc < 2) {
-    std::cerr << "Not enough arguments\nUsage: cpproll <filename>\n";
+    std::cerr << "Not enough arguments\n";
     exit(1);
   }
-  std::vector<std::string> args(argv + 1, argv + argc - 1);
-
-  std::string filename = std::filesystem::absolute(argv[1]);
-  std::vector<std::filesystem::path> sources = {filename};
-  if(!std::filesystem::exists(filename)) {
-    std::cerr << "The file \"" + filename + "\" doesn't exist\n";
+  std::vector<std::string> args(argv + 1, argv + argc);
+  std::vector<std::filesystem::path> sources;
+  std::string output_name = "a.out";
+  for(int i = 0; i < args.size(); i++) {
+    if(args[i] == "-o") {
+      i++;
+      if(i >= args.size()) {
+        std::cerr << "-o option used without a filename argument\n";
+        exit(1);
+      }
+      output_name = args[i];
+    } else {
+      sources.push_back(args[i]);
+    }
+  }
+  if(sources.empty()) {
+    std::cerr << "Not enough source files\nUsage: cpproll <filenames...>\nOptional arguments:\n\t-o <filename>\n";
     exit(1);
   }
   try {
-    roll(sources, "a.out");
+    roll(sources, output_name);
   } catch (std::filesystem::filesystem_error& e) {
     // TODO: better output
     std::cerr << e.what() << "\n";
