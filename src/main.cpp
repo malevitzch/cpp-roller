7719f8ca3cc86d10a10aee3ab44237734edcd5ac
malevitzch <malevitzch@gmail.com>
2026-06-12 11:20:57 +0200

    The option parsing is now done through getopt_long and supports long options in addition to regular single-character ones

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <vector>
 #include <filesystem>
+#include <getopt.h>
 #include "roller.hpp"
 
 int main(int argc, char** argv) {
@@ -10,31 +10,48 @@ int main(int argc, char** argv) {
   }
   if(argc < 2) {
     std::cerr << "Not enough arguments\n";
-    exit(1);
+    exit(EXIT_FAILURE);
   }
-  std::vector<std::string> args(argv + 1, argv + argc);
+
+  static option long_opts[] = {
+    {"help",    no_argument,       nullptr, 'h'},
+    {"version", no_argument,       nullptr, 'v'},
+    {"output",  required_argument, nullptr, 'o'},
+    {nullptr,   0,                 nullptr, 0},
+  };
 
   RollerConfig config;
-
-  for(int i = 0; i < args.size(); i++) {
-    if(args[i] == "-o") {
-      if(++i >= args.size()) {
-        std::cerr << "-o option used without a filename argument\n";
-        exit(1);
-      }
-      config.name(args[i]);
-    }
-    else if(args[i] == "-v") {
-      config.flag("version");
-    }
-    else {
-      config.add_source(args[i]);
+  int opt;
+  while ((opt = getopt_long(argc, argv, ":hvo:", long_opts, nullptr)) != -1) {
+    switch (opt) {
+      case 'h':
+        break;
+      case 'v':
+        config.flag("version");
+        break;
+      case 'o':
+        config.name(optarg);
+        break;
+      case ':':
+        std::cerr << "The option \"" << char(optopt) << "\" requires an argument\n";
+        exit(EXIT_FAILURE);
+      case '?': 
+        std::cerr << "Unknown option: \"" << char(optopt) << "\"\n";
+        exit(EXIT_FAILURE);
+      default:
+        std::cout << "Parse error: " << opt << "\n";
+        exit(EXIT_FAILURE);
     }
   }
+  for(int i = optind; i < argc; i++) {
+    config.add_source(argv[i]);
+  }  
+  
   try {
     roll(config);
   } catch (std::filesystem::filesystem_error& e) {
     // TODO: better output
     std::cerr << e.what() << "\n";
   }
+  return 0;
 }
