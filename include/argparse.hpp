2b99952bcd77bd772d86e8c75839032aed66d501
malevitzch <138793442+malevitzch@users.noreply.github.com>
2026-07-14 21:15:44 +0200

    The code now compiles on windows due to custom getopt alternative

---
+++ include/argparse.hpp
@@ -0,0 +1,3 @@
+#include "roller.hpp"
+
+RollerConfig parse_args(int argc, char** argv);
--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,7 @@
 #include <iostream>
-#include <getopt.h>
-#include <format>
 #include "roller.hpp"
 #include "common.hpp"
-
-RollerConfig parse_args(int argc, char** argv) {
-  static option long_opts[] = {
-    {"help",    no_argument,       nullptr, 'h'},
-    {"version", no_argument,       nullptr, 'v'},
-    {"output",  required_argument, nullptr, 'o'},
-    {"include", required_argument, nullptr, 'I'},
-    {nullptr,   0,                 nullptr, 0},
-  };
-
-  RollerConfig config;
-  int opt;
-  while ((opt = getopt_long(argc, argv, ":hvo:I:", long_opts, nullptr)) != -1) {
-    switch (opt) {
-      case 'h':
-        config.flag("help");
-        break;
-      case 'v':
-        config.flag("version");
-        break;
-      case 'o':
-        config.name(optarg);
-        break;
-      case 'I':
-        config.add_include_directories(optarg);
-        break;
-      case ':': {
-        throw CLIException(std::format("The option \"{}\" requires an argument", char(optopt)));
-      }
-      case '?':
-        throw CLIException(std::format("Unknown option \"{}\"", char(optopt)));
-      default:
-        throw CLIException(std::format("Unknown error while parsing option \"{}\"", char(optopt)));
-    }
-  }
-  for(int i = optind; i < argc; i++) {
-    config.add_source(argv[i]);
-  }
-  return config;
-}
-
+#include "argparse.hpp"
 
 int main(int argc, char** argv) {
   if constexpr(DEBUG) {
